77e03d7d61424384ffdbbc8f4f6360cacd7fb56d
Daniel Mutuku <danielmakau507@gmail.com>
2023-09-19 17:58:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,74 +1,51 @@
 // Code your solution in this file!
 //returns a distance in blocks
 function distanceFromHqInBlocks(someValue){
-    switch (someValue) {
-        case 43:
-            return(1)
-            break;
-        case 50:
-            return(8)
-            break;
-        case 34:
-            return(8)
-            break;
-        default:
-            break;
-    }
+    const hqBlock = 42
+    let diffBlock = Math.abs(hqBlock - someValue)//calculate number of blocks
+    return(diffBlock)
 }
 //Define distance from hq in blocks
-const someValue = 43
+distanceFromHqInBlocks(43)
+distanceFromHqInBlocks(50)
+distanceFromHqInBlocks(34)
 //returns a distance from hq in feet
-const blockValue = distanceFromHqInBlocks(someValue)
-function distanceFromHqInFeet(blockValue){
-    switch (blockValue) {
-        case 43:
-            return(264)
-            break;
-        case 50:
-            return(2112)
-            break;
-        case 34:
-            return(2112)
-            break;
-        default:
-            break;
-    }
+function distanceFromHqInFeet(someValue){
+    const hqBlock = 42
+    let diffBlock = Math.abs(hqBlock - someValue)//calculate number of blocks
+    const blockFeet = (diffBlock * 264)//calculate feet value
+    return blockFeet
 }
 //returns the distance travelled in feet
 function distanceTravelledInFeet(start, destination){
-    switch (start, destination) {
-        case 43, 48:
-            return(1320)
-            break;
-        case 50,60:
-            return(2640)
-            break;
-        case 34, 28:
-            return(1584)
-            break;
-        default:
-            break;
-    }
+    let travelPassenger = Math.abs(start - destination)//calculate travel in blocks
+    let travelPassengerFeet = (travelPassenger * 264)
+    return travelPassengerFeet
 }
 //Define start and destination values
-const start = 43
-const destination = 48
+distanceTravelledInFeet(43, 48)
+distanceTravelledInFeet(50, 60)
+distanceTravelledInFeet(34, 28)
 //Calculate fare price
 function calculatesFarePrice(start, destination){
-    switch (start, destination) {
-        case 43, 44:
-            return(0)
-            break;
-        case 34, 32:
-            return(2.56)
-            break;
-        case 50, 58:
-            return(25)
-            break;
-        case 34, 24:
-            return('cannot travel that far')
-            break;
-        default:
-            break;
+    const blockDistance = Math.abs(start -destination)
+    const feetDistance = (blockDistance * 264)
+    const distancePay = (feetDistance - 400)
+    let fare;
+    if(feetDistance <= 400 && feetDistance >= 0){
+        fare = 0 
+        return fare
+    }else if(feetDistance > 400 && feetDistance <= 2000){
+        fare = (distancePay * 0.02)
+        return fare
+    }else if(feetDistance > 2000 && feetDistance < 2500){
+        fare = 25
+        return fare
+    }else{
+        return('cannot travel that far')
     }
 }
+calculatesFarePrice(43, 44)
+calculatesFarePrice(34, 32)
+calculatesFarePrice(50, 58)
+calculatesFarePrice(34, 24)
